465a6925a7534dc7e7f740669684e8075a4bfa55
themosthandsome18 <amarillo.ralphrihmond.dll@gmail.com>
2026-09-17 03:45:00 +0800

    config(mobile): point default apiBaseUrl to live production Render backend

--- mobile/lib/utils/helpers.dart
+++ mobile/lib/utils/helpers.dart
@@ -2,7 +2,7 @@ part of '../main.dart';
 
 const apiBaseUrl = String.fromEnvironment(
   'API_BASE_URL',
-  defaultValue: 'http://127.0.0.1:8000/api',
+  defaultValue: 'https://capstone-backend-stzr.onrender.com/api',
 );
 
 const appModule = String.fromEnvironment('APP_MODULE', defaultValue: 'tourism');
